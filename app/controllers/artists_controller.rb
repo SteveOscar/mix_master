f046b9ea51fd0c886ea40f9a3b7b4fdbe327a7e3
Steven Olson <steveoscaro@gmail.com>
2016-01-03 18:42:20 -0700

    life raft time

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,8 @@
 class ArtistsController < ApplicationController
+  before_action :artist, only: [:show, :edit, :update, :destroy]
 
   def index
+    @artists = Artist.all
   end
 
   def new
@@ -13,26 +15,26 @@ class ArtistsController < ApplicationController
   end
 
   def edit
-    @artist = Artist.find(params[:id])
   end
 
   def update
-    @artist = Artist.find(params[:id])
-    @artist.update_attributes(artist_params)
-    redirect_to artist_path(@artist)
+    result = @artist.update_attributes(artist_params)
+    result ? (redirect_to artist_path(@artist)) : (render :edit)
   end
 
   def show
-    @artist = Artist.find(params[:id])
   end
 
   def destroy
-    @artist = Artist.find(params[:id])
     @artist.destroy
     redirect_to artists_path
   end
 
 private
+
+  def artist
+    @artist = Artist.find(params[:id])
+  end
 
   def artist_params
     params.require(:artist).permit(:name, :image_path)
